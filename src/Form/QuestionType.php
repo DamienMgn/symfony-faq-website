a8967b99f2a9244d5e533ba3fb9a3a8bda4ae77c
DamienMQ <maquignondm@gmail.com>
2019-09-17 14:08:27 +0200

    styles / form / header

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -2,23 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Tag;
 use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class QuestionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('body', TextareaType::class)
+            ->add('title', TextType::class, [
+                'required' => true
+            ])
+            ->add('body', TextareaType::class, [
+                'required' => true
+            ])
             ->add('tags')
             ->add('save', SubmitType::class)
         ;
